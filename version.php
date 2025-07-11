4528525efb1526b6ff182b5cb78259ead90e900c
brianlmerritt <brianlmerritt@noreply.com>
2025-07-11 14:29:08 +0100

    New ICON

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_eunitylink';
-$plugin->version = 2025071105;
+$plugin->version = 2025071108;
 $plugin->requires = 2023100900; // Moodle 4.5
-$plugin->maturity = MATURITY_RELEASE_CANDIDATE;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->release = '0.4.0'; 
